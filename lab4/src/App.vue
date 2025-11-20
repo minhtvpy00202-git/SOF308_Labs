b080639d763d68c1ce4f5c0087bc3308c6dc3966
minhtvpy00202 <minhtvpy00202@gmail.com>
2025-11-20 20:04:19 +0700

    Lab4

---
+++ lab4/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Hello from './components/Hello.vue'
+import bai3 from '../src/components/bai3.vue'
+import bai4 from '../src/components/bai4.vue'
+</script>
+
+<template>
+    <Hello></Hello>
+    <bai3></bai3>
+    <bai4></bai4>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ lab4/src/components/bai3.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- Hiển thị lời chào -->
+<h1>{{ greeting }}</h1>
+<!-- Tạo nút thay đổi nội dung -->
+ <button v-bind:title="buttonTitle" @click="changeGreeting">Click để thay
+đổi</button>
+</template>
+<script setup>
+import { ref } from 'vue';
+const buttonTitle = ref('xin chào')
+const greeting = ref('Lời chào ban đầu')
+const changeGreeting = () => {
+buttonTitle.value = 'Hi'
+greeting.value = 'Lời chào đã được thay đổi'
+}
+</script>
